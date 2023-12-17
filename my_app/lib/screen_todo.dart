bfc27df80fe771e1d34251483ffc9fbc68a1eb7f
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-17 16:04:33 +0900

    todo

--- my_app/lib/screen_todo.dart
+++ my_app/lib/screen_todo.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 class ScreenTodo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<int> listing = <int>[1, 2, 3, 4, 5];
     return Scaffold(
       appBar: AppBar(
         title: Text('TODO LIST'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to Screen A when the button   is pressed
-            Navigator.pop(context);
-          },
-          child: Text('Go back to Screen A'),
-        ),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => ListTile(title: Text('Item #$index')),
+              childCount: 1000,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
